ca153f15fb205ed5a31aaa84717b63af16a9f1e1
Huan Nguyen <cyraxnguyen@gmail.com>
2020-02-22 20:38:54 -0800

    Added Javadoc for GameInstance.java

--- src/Game2048/Game/GameInstance.java
+++ src/Game2048/Game/GameInstance.java
@@ -10,6 +10,10 @@ public class GameInstance implements Runnable {
     private static int numberOfThreads = 0;
     private static int[] winCount = new int[16];
     private static int depth = 3;
+
+    /**
+     * Play the game with AI enabled on a separate thread.
+     */
     @Override
     public void run() {
         // If there is more game to play then play the game, if not then stop the thread
@@ -43,6 +47,12 @@ public class GameInstance implements Runnable {
         }
     }
 
+    /**
+     * Initialize information that will be shared between threads.
+     * @param depth depths of search that Expectiminimax will perform
+     * @param numberOfGames number of games to be played
+     * @param numberOfThreads number of threads that will be spawned
+     */
     public static synchronized void initialize(int depth, int numberOfGames, int numberOfThreads) {
         GameInstance.depth = depth;
         GameInstance.numberOfGames = numberOfGames;
@@ -50,14 +60,25 @@ public class GameInstance implements Runnable {
         GameInstance.numberOfThreads = numberOfThreads;
     }
 
+    /**
+     * Set number of games to be played
+     * @param numberOfGames number of games to be played
+     */
     public static synchronized void setNumberOfGames(int numberOfGames) {
         GameInstance.numberOfGames = numberOfGames;
     }
 
+    /**
+     * Increment the number of games played.
+     */
     public static synchronized void incrementNumberOfGamesPlayed() {
         GameInstance.numberOfGamesPlayed++;
     }
 
+    /**
+     * Fetch the next game number and increment the number of games played.
+     * @return next game's number, -1 if all games are currently played
+     */
     public static synchronized int fetchNextGame() {
         if (!(numberOfGamesPlayed == numberOfGames)) {
             GameInstance.numberOfGamesPlayed++;
@@ -66,10 +87,17 @@ public class GameInstance implements Runnable {
         else return -1;
     }
 
+    /**
+     * Record the largest tile achieved from the given game
+     * @param largest largest tile achieved from the given game
+     */
     public static synchronized void recordLargestAchieved(int largest) {
         GameInstance.winCount[largest]++;
     }
 
+    /**
+     * Print out stats: Number of tiles achieved for the given tile value.
+     */
     public static synchronized void printStatistics() {
         System.out.println("Statistic: ");
         for (int i = 0; i < 16; i++) {
